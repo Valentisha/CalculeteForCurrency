8a27ff7efc11adf222117f3112e061592e37de5d
Valentina <moskwitina.v@yandex.ru>
2022-02-01 13:54:41 +0300

    калькулятор первый

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,58 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class='base'>
+<ul class='base-row'>
+<li class="cell1"><div class="content1" >
+    <select>
+  <option value="" >Доллар</option>
+  <option value="">Евро</option>
+    </select>
+    <br>
+    <input v-model='dollars' type='number' placeholder="введите количество"/>
+    <!-- <button @click='calculeteDollars' >Посчитать</button> -->
+    </div></li>
+<li class="cell1"><div class="content2">Рублей:
+    <p>{{rublesAmount}}</p>
+    </div></li>
+</ul>
+<p>Если заплатишь {{dollars}} долларов - получишь {{rublesAmount}} рублей по курсу {{dollarsCours}} за доллар</p>
+<p>Если заплатишь {{euro}} евро - получишь {{rubles}} рублей по курсу {{euroCours}} за евро</p>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    data(){
+        return {
+            rubles: 0,
+            dollars: 0,
+            euro: 0,
+            dollarsCours: 77,
+            euroCours: 86,
+
+
+        }
+    },
+    methods:{
+        calculeteDollars (){
+            this.rubles = this.dollarsCours * this.dollars;   
+        },
+        calculeteEuro(){
+            this.rubles = this.euroCours * this.euro;
+        },
+        
+    },
+    computed:{
+        rublesAmount:{
+            get(){
+                return this.dollars * this.dollarsCours
+            }
+        } 
+    }
+
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style >
 </style>
+
+
